d48059a00acaad746c2faeedbad7426a67fa2d26
mgardner3901 <31485789+mgardner3901@users.noreply.github.com>
2018-10-11 20:25:46 -0400

    Updated from Brython Server: 10/11/2018 8:25:46 PM

--- distribution.py
+++ distribution.py
@@ -1,8 +1,7 @@
 """
 distribution.py
-Author: <your name here>
-Credit: <list sources used, if any>
-
+Author: Morgan Gardner
+Credit: None
 Assignment:
 
 Write and submit a Python program (distribution.py) that computes and displays 
@@ -36,3 +35,33 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+
+userInput = input("Please enter a string of text (the bigger the better): ")
+print("The distribution of characters in " + '"' + userInput + '"'+ " is:")
+userInput = userInput.lower()
+
+characters = {"a":"","b":"","c":"","d":"","e":"","f":"","g":"","h":"","i":"","j":"","k":"","l":"","m":"","n":"","o":"","p":"","q":"","r":"","s":"","t":"","u":"","v":"","w":"","x":"","y":"","z":""}
+inputList = []
+
+for s in userInput:
+    if s != " " and s != "," and s != "?" and s != "!" and s != "." and s != "'" and s != ";" and s != ":":
+        characters[s]+=s
+
+"""
+for s in characters[s]:
+    if s == len(userInput):
+        print(characters[s])
+characters.sort(lambda x,y: cmp(len(x), len(y))) 
+"""
+charlist = []
+for key in characters:
+    if len(characters[key]) > 0:
+        charlist.append(characters[key])
+    
+charlist.sort()
+
+for l in range(len(userInput),0,-1):
+    for d in charlist:
+        if len(d) == l:
+            print(d)
+
